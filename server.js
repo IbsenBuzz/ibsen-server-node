adffa09071578a3d95857dfedb61982ec9b464f6
Luis Enrique Suárez <luissuarezdev@gmail.com>
2016-05-06 12:11:07 -0400

    server io

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ var $express = require('express');
 var $path    = require('path');
 var $app     = $express();
 var $server  = require('http').createServer($app);
+var $io      = require('socket.io').listen($server);
 var $config  = {
     PORT:   3002,
     SERVER: '127.0.0.1'
@@ -20,6 +21,25 @@ $app.use('/static', $express.static(__dirname + '/public'));
 $app.set('views', $path.join(__dirname, 'views'));
 
 $app.use('/', require('./routes/server'));
+
+$io.on('connection', function ($socket) {
+    $socket.emit('IbsenBuzz::server connection', {
+        id:        $socket.id,
+        connected: true
+    });
+
+    $socket.on('IbsenBuzz::server buzz_me', function (data) {
+        $socket.emit(data.nsp, data.data);
+    });
+
+    $socket.on('IbsenBuzz::server buzz', function (data) {
+        $socket.broadcast.emit(data.nsp, data.data);
+    });
+
+    $socket.on('disconnect', function () {
+        console.log('Disconnect IbsenBuzz::server IO :(');
+    });
+});
 
 $server.listen($config.PORT, $config.SERVER, function () {
     console.log('IbsenBuzz::server is running and listening to port %s:%d...', $config.SERVER, $config.PORT);
